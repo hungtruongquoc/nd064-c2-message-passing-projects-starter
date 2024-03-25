472165dd89c65148af6552d1541431945f728ac2
Hung Truong <hungtruongquoc@gmail.com>
2024-03-25 12:42:25 -0500

    20230325: Added PersonService using gRPC

--- modules/person_service/app/__init__.py
+++ modules/person_service/app/__init__.py
@@ -1,26 +1,32 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
-from flask_restx import Api
+from concurrent import futures
+
+import grpc
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
+from modules.person_service.app.udaconnect.services import PersonServicer
+from modules.shared.proto import person_pb2_grpc
 
 db = SQLAlchemy()
 
 
 def create_app(env=None):
-    from app.config import config_by_name
-    from app.routes import register_routes
+    from config import config_by_name
 
     app = Flask(__name__)
     app.config.from_object(config_by_name[env or "test"])
-    api = Api(app, title="UdaConnect API", version="0.1.0")
 
-    CORS(app)  # Set CORS for development
-
-    register_routes(api, app)
     db.init_app(app)
+    # Initialize gRPC server
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
+    person_pb2_grpc.add_PersonServiceServicer_to_server(PersonServicer(), server)
 
-    @app.route("/health")
-    def health():
-        return jsonify("healthy")
+    server.start()
+    server.add_insecure_port("[::]:5000")
+    server.wait_for_termination()
 
     return app
+
+
+if __name__ == "__main__":
+    create_app()
